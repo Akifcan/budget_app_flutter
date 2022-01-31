8e6375a2532027ea11d07311f802840ae68b101d
Akifcan Kara <48747986+Akifcan@users.noreply.github.com>
2022-02-01 00:36:46 +0300

    button added

--- lib/widgets/graphics/order_wallet.dart
+++ lib/widgets/graphics/order_wallet.dart
@@ -1,7 +1,9 @@
 import 'package:budget/db/models/wallet.dart';
 import 'package:budget/db/tables/wallet_table.dart';
+import 'package:budget/style.dart';
 import 'package:budget/widgets/loader.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class OrderWallet extends StatelessWidget {
   final String title;
@@ -18,8 +20,11 @@ class OrderWallet extends StatelessWidget {
       builder: (BuildContext context, AsyncSnapshot<List<Wallet>> snapshot) {
         return snapshot.hasData
             ? Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(title, style: Theme.of(context).textTheme.headline6),
+                  Center(
+                      child: Text(title,
+                          style: Theme.of(context).textTheme.headline6)),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: ListView.builder(
@@ -36,6 +41,12 @@ class OrderWallet extends StatelessWidget {
                           );
                         }),
                   ),
+                  TextButton.icon(
+                      onPressed: () {},
+                      icon:
+                          const Icon(FontAwesomeIcons.eye, color: primaryColor),
+                      label: const Text("Tüm Kayıtlar",
+                          style: TextStyle(color: primaryColor)))
                 ],
               )
             : const Loader();
